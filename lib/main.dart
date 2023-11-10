0c141bb728d393db2e6755ff57689151c218c733
topcircle <topcircle@gmail.com>
2023-11-10 12:30:38 +0900

    ##### #5.1 AnimatedSwitcher

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animations_masterclass/screens/apple_watch_screen.dart';
-import 'package:flutter_animations_masterclass/screens/explicit_animations_screen.dart';
-import 'package:flutter_animations_masterclass/screens/implicit_animations_screen.dart';
-import 'package:flutter_animations_masterclass/screens/music_player_screen.dart';
-import 'package:flutter_animations_masterclass/screens/swiping_cards_screen.dart';
+import 'package:flutter_animations_masterclass/screens/menu_screen.dart';
 
-class MenuScreen extends StatelessWidget {
-  const MenuScreen({super.key});
+void main() {
+  runApp(const MyApp());
+}
 
-  void _goToPage(BuildContext context, Widget screen) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => screen,
-      ),
-    );
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Animations'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                _goToPage(
-                  context,
-                  const ImplicitAnimationsScreen(),
-                );
-              },
-              child: const Text('Implicit Animations'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                _goToPage(
-                  context,
-                  const ExplicitAnimationsScreen(),
-                );
-              },
-              child: const Text('Explicit Animations'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                _goToPage(
-                  context,
-                  const AppleWatchScreen(),
-                );
-              },
-              child: const Text('Apple Watch'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                _goToPage(
-                  context,
-                  const SwipingCardsScreen(),
-                );
-              },
-              child: const Text('Swiping Cards'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                _goToPage(
-                  context,
-                  const MusicPlayerScreen(),
-                );
-              },
-              child: const Text('Music Player'),
-            ),
-          ],
+    return MaterialApp(
+      title: 'Flutter Animations Masterclass',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: const ColorScheme.light(
+          primary: Colors.blue,
         ),
       ),
+      home: const MenuScreen(),
     );
   }
 }
